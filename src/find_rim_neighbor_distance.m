e86288182ba6320dcff64d9b5bed9b0bf4e0178e
abulnaga <abulnaga@mit.edu>
2019-12-04 18:48:36 -0500

    rim estimation with less strict requirements

--- src/find_rim_neighbor_distance.m
+++ src/find_rim_neighbor_distance.m
@@ -7,7 +7,7 @@ function [IDX, nNodes] = find_rim_neighbor_distance(A, P, clusterIDX, neighborDi
 %   A: adjacency matrix (sparse) AKA 1-ring neighborhood
 %   P: nvx3: list of vertex coordinate positions
 %   clusterIDX: matrix containing 1 and 2, indicating clutser of fetal or
-%   maternal side. If it contains 0 or 1 instead, it will convert it.
+%       maternal side. If it contains 0 or 1 instead, it will convert it.
 %   neighborDist: the distance threshold for finding the neighbors of a
 %   node. I.e., all nodes with a shortest path < neighborDist are
 %   considered in the neighborhood when it comes to finding the rim.
@@ -20,67 +20,73 @@ D = compute_euclidean_distance_neighbors(P,A,[],0);
 G = graph(D);
 d = distances(G);
 
+voronoiAreas = voronoi_area(T, unique(T), P);
+voronoiAreas = voronoiAreas / sum(voronoiAreas);
+
 neighborDistO = neighborDist;
+neighborDistThresh = 0.25; %comes from examining rim of 105 subjects.
 addDist = 0;
 %check if input index list has 2 clusters
 nEl = length(unique(clusterIDX));
 if(nEl ~=2)
     error('more than 2 clusters inputted')
 else %check if 0 or 1 inidices.
-   i = find(nEl ==0);
-   clusterIDX(i) = 2;
+    i = find(nEl ==0);
+    clusterIDX(i) = 2;
 end
 %Here, compute the rim, and then run some checks.
 computeRim = 1;
 count = 0;
+countBreak = 0;
 while(computeRim)
-   Adist = zeros(size(A));
-   for i = 1:length(A)
-       ids = find(d(i,2:end)<=neighborDist);
-       ids = ids+1;
-       Adist(i,ids) = 1;
-   end
+    Adist = zeros(size(A));
+    for i = 1:length(A)
+        ids = find(d(i,2:end)<=neighborDist);
+        ids = ids+1;
+        Adist(i,ids) = 1;
+    end
     IDX = clusterIDX;
-    IDX(find_rim_from_2_cluster(Adist,clusterIDX)==1) = 3; 
-    %trisurf(T,P(:,1),P(:,2),P(:,3),IDX);
-    %trisurf(T,P(:,1),P(:,2),P(:,3),clusterIDX);
+    IDX(find_rim_from_2_cluster(Adist,clusterIDX)==1) = 3;
+    
     %now, have the rim. Run some checks. Mainly, that have at least 30% of
     %nodes on the rim (maybe remove later), and that we dont cut the fetal
     %or maternal sides.
-    nNodes = sum(IDX==3)/length(IDX);
+    nNodes = sum(voronoiAreas(IDX==3));
     binA = conncomp(graph(adjacency_matrix_per_label(A,IDX,1)));
     binB= conncomp(graph(adjacency_matrix_per_label(A,IDX,2)));
     binRim = conncomp(graph(adjacency_matrix_per_label(A,IDX,3)));
     numConA = length(unique(binA(IDX==1)));
     numConB = length(unique(binB(IDX==2)));
     numConRim = length(unique(binRim(IDX==3)));
-
-    if(numConA ~=1 || numConB ~=1)
-        computeRim = 1;
-        if(addDist == 0)
-            neighborDist = neighborDist - 0.5;
-        else
-            neighborDist = neighborDist + 0.5;
-        end
-        warning('rim breaks up fetal or maternal side');
-%     elseif(numConRim ~=1)
-%         computeRim=1;
-%         neighborDist = neighborDist + 0.5;
-%         warning('rim broken up (non conitnuous');
-    elseif(nNodes < 0.30) %probably should remove this condition!
-        if(count < 10)            
+    if(countBreak == 0)
+        if(numConA ~=1 || numConB ~=1)
             computeRim = 1;
-            neighborDist = neighborDist+0.25;
-            warning('too few nodes <threshold %');
+            if(addDist == 0)
+                neighborDist = neighborDist - 0.25;
+            else
+                neighborDist = neighborDist + 0.25;
+            end
+            warning('rim breaks up fetal or maternal side');
+        elseif(nNodes < neighborDistThresh) %probably should remove this condition!
+            if(count <= 5)
+                computeRim = 1;
+                neighborDist = neighborDist+0.25;
+                warning('too few nodes <threshold %');
+            else
+                computeRim = 0;
+            end
         else
-           computeRim = 0; 
+            computeRim = 0;
+        end
+        if(neighborDist <= 0 )
+            neighborDist = neighborDistO;
+            addDist = 1;
+        elseif(neighborDist >=10)
+            neighborDist = neighborDistO;
+            countBreak = 1;
         end
     else
         computeRim = 0;
-    end
-    if(neighborDist <= 0 )
-       neighborDist = neighborDistO;
-       addDist = 1;
     end
     count = count + 1;
 end
